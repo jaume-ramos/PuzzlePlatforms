1083f2a348c0354a11b020348bb94ac1162d74a2
Jaume Ramos Moreso <jaume.ramosmoreso@gmail.com>
2021-09-23 11:26:18 +0200

    Added replicated movable platforms
    Implemented a platform that moves between two points. The platform is replicated, so when the server updates it's location sends the new position to all the clients.

---
+++ Source/PuzzlePlatforms/MovingPlatform.cpp
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MovingPlatform.h"
+
+AMovingPlatform::AMovingPlatform() {
+	
+	PrimaryActorTick.bCanEverTick = true;
+
+	movingSpeed = 5.f;
+
+	SetMobility(EComponentMobility::Movable);
+}
+
+void AMovingPlatform::BeginPlay() {
+
+	Super::BeginPlay();
+
+	if (HasAuthority()) {
+		SetReplicates(true);
+		SetReplicateMovement(true);
+	}
+
+	// Store the initial location of the target
+	GlobalStartLocation = GetActorLocation();
+	// Convert local target coordinates to world coordinates
+	GlobalTargetLocation = GetTransform().TransformPosition(TargetLocation);
+}
+
+void AMovingPlatform::Tick(float DeltaTime) {
+
+	Super::Tick(DeltaTime);
+
+	
+	if (HasAuthority()) {
+		FVector Location = GetActorLocation();
+		
+		float JourneyLength = (GlobalTargetLocation - GlobalStartLocation).Size();
+		float JourneyTravelled = (Location - GlobalStartLocation).Size();
+
+		if (JourneyTravelled >= JourneyLength) {
+
+			FVector swapTemp = GlobalStartLocation;
+			GlobalStartLocation = GlobalTargetLocation;
+			GlobalTargetLocation = swapTemp;
+		}
+
+		FVector Direction = (GlobalTargetLocation - GlobalStartLocation).GetSafeNormal();
+		
+		Location += movingSpeed * DeltaTime * Direction;
+
+		SetActorLocation(Location);
+	}	
+}
---
+++ Source/PuzzlePlatforms/MovingPlatform.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/StaticMeshActor.h"
+#include "MovingPlatform.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PUZZLEPLATFORMS_API AMovingPlatform : public AStaticMeshActor
+{
+	GENERATED_BODY()
+	
+public:
+	AMovingPlatform();
+
+protected:
+	
+	virtual void BeginPlay() override;
+	
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere)
+	float movingSpeed;
+
+	UPROPERTY(EditAnywhere, Meta = (MakeEditWidget = true))
+	FVector TargetLocation;
+
+
+private:
+	FVector GlobalTargetLocation;
+	FVector GlobalStartLocation;
+
+};
